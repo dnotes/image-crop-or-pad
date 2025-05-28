dac1ffcf0ea3ce17d44f068cbb4fc11b54a992a5
David Hunt <git@dnotes.net>
2025-05-28 16:15:20 +1200

    add mdsvex for content

---
+++ src/lib/Prose.svelte
@@ -0,0 +1,3 @@
+<div class="prose prose-sm prose-p:leading-snug prose-p:text-justify max-w-4xl">
+  <slot></slot>
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,11 @@
 <script>
 	import '../app.css';
-	
+
 	let { children } = $props();
 </script>
+
+<svelte:head>
+  <title>image-crop-or-pad is a tiny isomorphic JS image resizer for visual regression testing</title>
+</svelte:head>
 
 {@render children()}
