b24ff007085a72ce9378edf61eb1681803a93525
David Hunt <git@dnotes.net>
2025-06-02 11:37:23 +1200

    fix: adjust header padding and update navigation links

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,13 +9,14 @@
 </svelte:head>
 
 <div class="flex w-screen h-screen flex-col gap-8">
-	<header class="flex gap-2 items-center justify-between sticky top-0 bg-gray-200 px-5 lg:px-12 py-2 lg:py-8">
+	<header class="flex gap-2 items-center justify-between sticky top-0 bg-gray-200 px-5 lg:px-12 py-2 lg:py-5">
 		<h1 class="text-xl font-bold lg:text-4xl">
 			<a href="/">image-crop-or-pad</a>
 		</h1>
-		<nav class="flex gap-2">
-			<a href="/">Playground</a>
-			<a href="/about">About</a>
+		<nav class="flex gap-2 md:gap-6">
+			<a href="https://www.npmjs.com/package/image-crop-or-pad">npm</a>
+			<a href="/">playground</a>
+			<a href="/about">about</a>
 		</nav>
 	</header>
 	<main class="flex-grow px-5 lg:px-12 justify-center">
